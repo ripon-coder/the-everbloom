3b89c2b048b342e3df241c475dc4419148cf3e04
Ripon Shikder <coderripon@gmail.com>
2025-05-07 19:44:29 +0600

    add blade directive

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,13 +3,14 @@
 namespace App\Providers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Support\Facades\RateLimiter;
 use App\Repositories\Eloquent\AdminRepository;
+use App\Repositories\Eloquent\BrandRespository;
 use App\Repositories\Contracts\AdminRepositoryInterface;
 use App\Repositories\Contracts\BrandRespositoryInterface;
-use App\Repositories\Eloquent\BrandRespository;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -30,6 +31,10 @@ class AppServiceProvider extends ServiceProvider
     {
         RateLimiter::for('global', function (Request $request) {
             return Limit::perMinute(60);
+        });
+        
+        Blade::directive('active', function ($expression) {
+            return "<?php echo request()->is({$expression}) ? 'active' : ''; ?>";
         });
     }
 }
--- resources/views/admin/brand/index.blade.php
+++ resources/views/admin/brand/index.blade.php
@@ -45,12 +45,12 @@
                                 </a>
                             </div>
                             <div class="col-auto">
-                                <a class="btn app-btn-secondary" href="{{route('admin.product.create')}}">
+                                <a class="btn app-btn-secondary" href="{{route('admin.brand.create')}}">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" class="bi bi-plus-circle me-1" viewBox="0 0 16 16">
                                         <path d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zM8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0z"/>
                                         <path d="M8 4a.5.5 0 0 1 .5.5V7.5H11.5a.5.5 0 0 1 0 1H8.5V11.5a.5.5 0 0 1-1 0V8.5H4.5a.5.5 0 0 1 0-1H7.5V4.5A.5.5 0 0 1 8 4z"/>
                                       </svg>
-                                    New Product
+                                    New Brand
                                 </a>
                             </div>
                         </div><!--//row-->
