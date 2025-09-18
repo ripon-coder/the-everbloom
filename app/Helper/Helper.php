f8acf8db1241a8bde529982b7800b2187a41afbf
coder-ripon <coderripon@gmail.com>
2025-09-18 15:04:22 +0600

    all thing delete

--- app/Helper/Helper.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace App\Helper;
-class Helper{
-    public static function DateFormat($date){
-        return date("d-m-Y", strtotime($date));
-    }
-}
--- app/Repositories/Contracts/AdminRepositoryInterface.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace App\Repositories\Contracts;
-use App\Models\Admin;
-interface AdminRepositoryInterface{
-    public function logedIn($request);
-    public function logOut();
-}
